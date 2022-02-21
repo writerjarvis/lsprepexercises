d050254b9870f43e4b88bd071ca636cb807d226e
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-02-21 18:55:43 +0000

    exFour.js

---
+++ basics/exFour.js
@@ -0,0 +1,4 @@
+/* Explain why this code logs '510' instead of 15.
+	 console.log ('5' + 10); */
+
+// '5' is a string, so JavaScrit performs string concatentation and joins '5' a string and 10 as though it is a string. This is implicit coercion//
