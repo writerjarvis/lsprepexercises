99f61dfbf47fdad68f426d7d58666290ba1a82b3
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-02 16:07:51 +0000

    ex-one.js

---
+++ flow-control/ex-one.js
@@ -0,0 +1,29 @@
+/* What values do the following expressions evaulate to?
+
+false || (true && false);
+true || (1 + 2);
+(1 + 2) || true;
+true && (1 + 2);
+false && (1 + 2);
+(1 + 2) && true;
+(32 * 4) >= 129;
+false !== !true;
+true === 4;
+false === (847 === '847');
+false === (847 == '847');
+(!true || (!(100 / 5) === 20) || ((328 / 4) === 82)) || false; 		*/
+
+/* answer below:
+False
+True
+3
+3
+False
+True
+False
+False
+False
+True
+False
+True
+*/
