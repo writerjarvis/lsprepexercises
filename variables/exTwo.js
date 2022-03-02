258100813e6b330a1671d957629fd58d8c9914a4
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-02 14:34:14 +0000

    exTwo.js

---
+++ variables/exTwo.js
@@ -0,0 +1,10 @@
+// Write a program named age.js that includes someone's age and then calculates and reports the future age in 10,20,30 and 40 years.
+
+// Answer below
+
+let userAge = 27;
+console.log(`You are ${userAge}, years old.`);
+console.log(`In 10 years, you will be ${userAge + 10} years old.`);
+console.log(`In 20 years, you will be ${userAge + 20} years old.`);
+console.log(`In 30 years, you will be ${userAge + 30} years old.`);
+console.log(`In 40 years, you will be ${userAge + 40} years old.`);
