f8853ff3fbd772492bc50b6c4ca7937b9506c32b
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-02 17:21:23 +0000

    ex-eight.js

---
+++ flow-control/ex-eight.js
@@ -0,0 +1,15 @@
+/* Write a functiont that logs wheter a number is between 0 and 50 (inclusive), between 51 and 100 (inclusive), greater than 100, or less than 0 
+
+Answer below: */
+
+function numberRange(number) {
+  if (number < 0) {
+    console.log(`${number} is less than 0`);
+  } else if (number <= 50) {
+    console.log(`${number} is between 0 and 50`);
+  } else if (number <= 100) {
+    console.log(`${number} is between 50 and 100`);
+  } else {
+    console.log(`${number} is greater than 100`);
+  }
+}
