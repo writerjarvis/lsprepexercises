e51fa049821b382ce66afc3598617cc2b12a7151
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-02 14:38:01 +0000

    exThree.js

---
+++ variables/exThree.js
@@ -0,0 +1,12 @@
+/* What happens when you run the following program? Why do we get that result?
+
+{
+	let foo = 'bar';
+}
+
+console.log(foo); */
+
+/* Answer below
+
+You will get an erro, foo is not defind. It is assigned to a variable in the block,
+but console.log(foo) is out of the block */
