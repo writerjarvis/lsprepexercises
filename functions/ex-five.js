a7df7f73dc8538dfcecec021dabb3e9b968cb020
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-02 15:49:44 +0000

    ex-five.js

---
+++ functions/ex-five.js
@@ -0,0 +1,12 @@
+/* What does the following log to the console? 
+
+function scream(words) {
+  return words + '!!!!';
+}
+
+scream('Yipeee');						*/
+
+
+/* Answer below 
+
+It returns the 'Yipeee' + !!!! as a return value, but there is no console.log so nothing is logged to the console */
