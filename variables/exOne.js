c9a88c39d22aad0271c918271230a318b32b882a
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-02 14:28:29 +0000

    exOne.js

---
+++ variables/exOne.js
@@ -0,0 +1,9 @@
+/* Write a program named greeter.js that greets 'Victor' three times.
+Your program should use a variable and not hard code the string value 'Victor' in each greeting */
+
+// answer below
+
+let firstName = 'Victor';
+console.log(`Good Morning, ${firstName}.`);
+console.log(`Good Afternoon, ${firstName}.`);
+console.log(`Good Evening, ${firstName}.`);
