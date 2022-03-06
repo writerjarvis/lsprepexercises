6c360e5db0bf305fdfc7d71c71c9b30db0bf3fbf
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-06 18:44:47 +0000

    ex-ten.js

---
+++ arrays/ex-ten.js
@@ -0,0 +1,20 @@
+/* Write some code to replace the value 6 in the followig array with 606 
+
+let arr = [
+  ["hello", "world"],
+  ["example", "mem", null, 6, 88],
+  [4, 8, 12]
+];
+
+You don't have to search the array. Just write an assignment that replaces the 6. 
+
+answer below: */
+
+let arr = [
+  ["hello", "world"],
+  ["example", "mem", null, 6, 88],
+  [4, 8, 12]
+];
+
+arr[1][3] = 606;
+
