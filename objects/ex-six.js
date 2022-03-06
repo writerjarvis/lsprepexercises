8fb661e7466e77de5b17aeb8ef2dc211bab3d938
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-06 19:33:07 +0000

    ex-six.js

---
+++ objects/ex-six.js
@@ -0,0 +1,12 @@
+/* Which of the following values are primitive values? Which are objects? Which are neither?
+
+"foo" - primitive 
+3.1415 - primitive 
+[ 'a', 'b', 'c' ] - objects
+false - primitive 
+foo - neither
+function bar() { return "bar"; } - objects
+undefined - primitive
+{ a: 1, b: 2 } - objects
+
+*/
