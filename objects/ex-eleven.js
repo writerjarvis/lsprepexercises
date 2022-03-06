dc4ef0392d5983d6fdfcd6268ab2f952c494ecf0
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-06 19:52:17 +0000

    ex-eleven.js

---
+++ objects/ex-eleven.js
@@ -0,0 +1,19 @@
+/* Write some code to replace the value 6 in the following object with 606:
+
+let obj = {
+  foo: { a: "hello", b: "world" },
+  bar: ["example", "mem", null, { xyz: 6 }, 88],
+  qux: [4, 8, 12]
+};
+
+You don't have to search the object. Just write an assignment that replaces the 6.
+
+answer below:	*/
+
+let obj = {
+  foo: { a: "hello", b: "world" },
+  bar: ["example", "mem", null, { xyz: 6 }, 88],
+  qux: [4, 8, 12]
+};
+
+obj.bar[3].xyz = 606;
