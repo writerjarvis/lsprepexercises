90a69b33a61ff961701a8381095335def4236c0c
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-06 19:10:09 +0000

    ex-one.js

---
+++ objects/ex-one.js
@@ -0,0 +1,17 @@
+/* Given the following code, how can you access the name of the person?
+
+let person = {
+	name:				'bob',
+	occupation: 'web developer',
+	hobbies: 		'painting',
+};
+
+answer below: */
+
+let person	= {
+	name: 				'bob',
+	occupatipn: 	'web developer',
+	hobbies:			'painting',
+}
+
+console.log(person['name']);
