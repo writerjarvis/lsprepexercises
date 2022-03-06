0daafdf5294438e91fba47250a0eb24b005b6732
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-06 19:50:24 +0000

    ex-ten.js

---
+++ objects/ex-ten.js
@@ -0,0 +1,17 @@
+/* How many primitive values are there in the following expression? Identify them.
+How many objects are there in the expression? Identify those objects.
+
+[1, 2, ["a", ["b", false]], null, {}]
+
+Answer below:
+
+There are 6 primitive values and 4 objects.
+
+Primitive values: 1, 2, "a", "b", false, null
+Objects: 
+[1, 2, ["a", ["b", false]], null, {}] 
+["a", ["b", false]]
+{}
+["b", false]
+
+*/
