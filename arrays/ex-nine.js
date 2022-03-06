99f14411f450ad763cf502c3bd7841e0e59ba692
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-06 18:42:28 +0000

    ex-nine.js

---
+++ arrays/ex-nine.js
@@ -0,0 +1,21 @@
+/* Without using a for, while, or do/while loop, write soem code that checks whether the  number 3 appears inside these arrays:
+
+> let numbers1 = [1, 3, 5, 7, 9, 11];
+> let numbers2 = [];
+> let numbers3 = [2, 4, 6, 8];  
+
+return true or false depending on each result.
+
+answer below */
+
+let numbers1 = [1, 3, 5, 7, 9, 11];
+let numbers2 = [];
+let numbers3 = [2, 4, 6, 8];  
+
+numbers1.includes(3);
+numbers2.includes(3);
+numbers3.includes(3);
+
+
+
+
