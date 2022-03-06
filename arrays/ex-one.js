9d816bde56adeddf6f2d1348a73c9345160042e2
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-06 17:32:40 +0000

    ex-one.js

---
+++ arrays/ex-one.js
@@ -0,0 +1,24 @@
+/* In the following code, what are the final values for array1, array 2, array 3, array 4, array 5? 
+
+let array1 = [1, 2, undefined, 4];
+
+let array2 = [1];
+array2.length = 5;
+
+let array3 = [];
+array3[-1] = [1];
+
+let array4 = [1, 2, 3, 4, 5];
+array4.length = 3;
+
+let array5 = [];
+array5[100] = 3;
+
+
+Answer below:
+
+Array 1 - 4
+Array 2 - 5
+Array 3 - 0
+Array 4 - 3
+Array 5 - 101				*/
