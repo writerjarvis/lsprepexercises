730c2a83e37aa61a729e8fb97255e97d1e25a50b
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-08 17:58:07 +0000

    ex-one.js

---
+++ more-stuff/ex-one.js
@@ -0,0 +1,10 @@
+/* What does the code log to the console? Why?
+
+let array1 = [1, 2, 3];
+let array2 = array1;
+array1[1] = 4;
+console.log(array2);
+
+answer below:
+
+It will log [1, 4, 3], on the 3rd line we change array1 and remove the 4 and replace it with a 2. */
