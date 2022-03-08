5baf96850fbf34211a1cf4984f9e21cb5a921292
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-08 18:42:27 +0000

    ex-seven.js

---
+++ more-stuff/ex-seven.js
@@ -0,0 +1,8 @@
+/* What is exception handling and what problem does it solve?
+
+Answer below: 
+
+Exception handling is a method that deals with errors in a manageable and predictable manner.
+It uses the try/catch statement to catch exception that the code in the try block raises.
+This lets the programmer deal with the problem in a way that makes sense. 			*/
+
