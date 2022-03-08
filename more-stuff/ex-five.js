94c7dcd16c9014d307061bc66b0d19aaab0c9f44
Jarvis Viskinda <viskindajarvis@protonmail.com>
2022-03-08 18:27:52 +0000

    ex-five.js

---
+++ more-stuff/ex-five.js
@@ -0,0 +1,9 @@
+/* What does the following function do?
+
+function doSomething(string) {
+	return string.split(' ').reverse().map((value) => value.length);
+}
+
+Answer below:
+
+Code converts a string into an array of words, reverses that array, then returns a new array that contains the lengths of the words. */
